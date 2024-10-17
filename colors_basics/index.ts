2e31266588d72aa3e604ab340bd7134d89f18c01
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 09:37:13 +0200

    Saved in TypeScript Playground at 2024-10-17T07:37:13.235Z

---
+++ colors_basics/index.ts
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(200, 200);
+
+
+  // Switch the color mode to RGB:
+  colorMode(RGB);
+
+  // Try different RGB values:
+  //         +------------ red (0..255)
+  //         |    +------- green (0..255)
+  //         |    |  +---- blue (0..255)
+  //         v    v  v
+  background(255, 0, 0);
+}
