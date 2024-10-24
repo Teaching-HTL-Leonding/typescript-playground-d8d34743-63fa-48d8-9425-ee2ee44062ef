e03834d4717ebbd8f58f99baad81d5977ffd7dbf
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 09:03:32 +0200

    Saved in TypeScript Playground at 2024-10-24T07:03:31.907Z

---
+++ random_cards/index.ts
@@ -0,0 +1,21 @@
+let cardImage: p5.Image;
+
+const BASE_URL = 'https://cddataexchange.blob.core.windows.net/images/cards';
+
+function preload() {
+  // Generate random color (0 = clubs, 1 = diamonds, 2 = hearts, 3 = spades)
+  const colorIx = Math.floor(random(0, 4));
+
+  // Generate random card (1 = Ace, 2 = 2, ..., 10 = 10, 11 = Jack, 12 = Queen, 13 = King)
+  const cardIx = Math.floor(random(1, 14));
+
+  // <<< Add code here
+}
+
+function setup() {
+  createCanvas(250, 250);
+  background("darkgreen");
+
+  imageMode(CENTER);
+  image(cardImage, width / 2, height / 2);
+}
