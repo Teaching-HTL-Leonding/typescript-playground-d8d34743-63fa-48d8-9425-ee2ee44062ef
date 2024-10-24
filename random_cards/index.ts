f073cd74b4bd5a51fb5fad95f85fc9553353d1b5
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 11:32:45 +0200

    Saved in TypeScript Playground at 2024-10-24T09:32:45.093Z

--- random_cards/index.ts
+++ random_cards/index.ts
@@ -5,17 +5,57 @@ const BASE_URL = 'https://cddataexchange.blob.core.windows.net/images/cards';
 function preload() {
   // Generate random color (0 = clubs, 1 = diamonds, 2 = hearts, 3 = spades)
   const colorIx = Math.floor(random(0, 4));
+  let colorName: string;
+
+  switch (colorIx) {
+    case 0:
+      colorName = "clubs"
+      break;
+    case 1:
+      colorName = "diamonds"
+      break;
+    case 2:
+      colorName = "hearts"
+      break;
+    case 3:
+      colorName = "spades"
+      break;
+  }
 
   // Generate random card (1 = Ace, 2 = 2, ..., 10 = 10, 11 = Jack, 12 = Queen, 13 = King)
   const cardIx = Math.floor(random(1, 14));
+  let cardName: string;
 
-  // <<< Add code here
-}
+  switch (cardIx) {
+    case 0:
+      cardName = "A"
+      break;
+    case 1:
+      cardName = "2"
+      break;
+    case 2:
+      cardName = "3"
+      break;
+    case 3:
+      cardName = "4"
+      break;
+    case 4:
+      cardName = "5"
+    case 5:
+      cardName = "6"
+      break;
+    case 1:
+      cardName = "2"
+      break;
+    case 1:
+      cardName = "2"
+      break;
+    case 1:
+      cardName = "2"
+      break;
+    case 1:
+      cardName = "2"
+      break; }
+      case
 
-function setup() {
-  createCanvas(250, 250);
-  background("darkgreen");
-
-  imageMode(CENTER);
-  image(cardImage, width / 2, height / 2);
 }
