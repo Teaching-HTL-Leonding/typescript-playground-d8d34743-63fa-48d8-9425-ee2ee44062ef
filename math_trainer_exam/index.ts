f52a31cd4232d08d8ad1cb4bb8af7958dee641cf
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 09:49:54 +0100

    Saved in TypeScript Playground at 2024-11-07T08:49:53.750Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -8,38 +8,49 @@ let randomOperand2 = 0
 let randomOperator = 0
 let operator = ''
 
-let ergebnis =
-    let randomPosition =
+let ergebnis = 0
+let randomPosition = Math.floor(random(0, 3))
 
-        let wrongAnswer1 =
-        let wrongAnswer2 =
+let wrongAnswer1 = Math.floor(random(-100, 250))
+let wrongAnswer2 = Math.floor(random(100, 500))
 
-        // setup function runs once and sets up the quiz elements on the canvas
-        function setup() {
-            randomOperand1 = Math.floor(random(1 - 101))
-            randomOperand2 = Math.floor(random(1 - 101))
+// setup function runs once and sets up the quiz elements on the canvas
+function setup() {
+    randomOperand1 = Math.floor(random(1 - 101))
+    randomOperand2 = Math.floor(random(1 - 101))
 
-            randomOperator = Math.floor(random(0, 3))
-            if (randomOperator = 0) {
-                operator = '+'
-                ergebnis = randomOperand1 + randomOperand2
+    randomOperator = Math.floor(random(0, 3))
+    if (randomOperator = 0) {
+        operator = '+'
+        ergebnis = randomOperand1 + randomOperand2
+    }
+    else if (randomOperator = 1) {
+        operator = '-'
+        ergebnis = randomOperand1 - randomOperand2
+    }
 
-            }
+    else {
+        operator = '*'
+        ergebnis = randomOperand1 * randomOperand2
+
+    }
+    if (randomPosition = 0) {
+        text(ergebnis, width / 6, height / 2)
+        text(wrongAnswer1, width / 3, height / 2)
+        text(wrongAnswer2, width / 3, height / 2)
+    }
+
+    createCanvas(400, 400);
+    background("black");
+
+    fill("yellow")
+    textSize(70)
+    text(randomOperand1, width / 4, 100)
+    text(randomOperand2, width / 2, 100)
 
 
 
-
-            createCanvas(400, 400);
-            background("black");
-
-            fill("yellow")
-            textSize(70)
-            text(randomOperand1, width / 4, 100)
-            text(randomOperand2, width / 2, 100)
-
-
-
-        }
+}
 
 // mouseClicked function checks if the user clicked on the correct answer
 function mouseClicked() {
