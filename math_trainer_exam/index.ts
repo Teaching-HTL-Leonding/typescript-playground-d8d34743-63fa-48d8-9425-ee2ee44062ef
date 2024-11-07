a464191f9a5d2cb117d5a7238621f5b56411d44f
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 08:29:53 +0100

    Saved in TypeScript Playground at 2024-11-07T07:29:53.291Z

---
+++ math_trainer_exam/index.ts
@@ -0,0 +1,34 @@
+// This program generates a simple math quiz in a graphical window, where the user selects the correct result
+// of a randomly generated arithmetic operation.
+
+// <<< TODO: Declare your GLOBAL variables here
+let randomOperator = 0
+
+
+// setup function runs once and sets up the quiz elements on the canvas
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+
+    // <<< TODO: Add your code here
+}
+
+// mouseClicked function checks if the user clicked on the correct answer
+function mouseClicked() {
+ let randomOperator: any // <<< TODO: Add your code here
+}
+
+// mouseMoved function displays mouse coordinates as feedback
+function mouseMoved() {
+    // This is a HELPER FUNCTION. It should make finding coordinates
+    // easier for you. You DO NOT NEED to change this method!
+
+    fill("black");
+    noStroke();
+    rect(0, height - 20, width, height); // Draws a rectangle to clear previous coordinates
+
+    fill("white");
+    textSize(10);
+    textAlign(LEFT, BOTTOM); // Aligns coordinates text to bottom-left
+    text(`${mouseX}/${mouseY}`, 5, height - 5); // Displays current mouse coordinates
+}
