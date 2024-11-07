77daeb23b06779bf752efa623dd61a4ec100ff8f
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 09:33:55 +0100

    Saved in TypeScript Playground at 2024-11-07T08:33:55.195Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -2,20 +2,48 @@
 // of a randomly generated arithmetic operation.
 
 // <<< TODO: Declare your GLOBAL variables here
+let randomOperand1 = 0
+let randomOperand2 = 0
+
 let randomOperator = 0
+let operator = ''
+
+let ergebnis =
+    let randomPosition =
+
+        let wrongAnswer1 =
+        let wrongAnswer2 =
+
+        // setup function runs once and sets up the quiz elements on the canvas
+        function setup() {
+            randomOperand1 = Math.floor(random(1 - 101))
+            randomOperand2 = Math.floor(random(1 - 101))
+
+            randomOperator = Math.floor(random(0, 3))
+            if (randomOperator = 0) {
+                operator = '+'
+                ergebnis = randomOperand1 + randomOperand2
+
+            }
 
 
-// setup function runs once and sets up the quiz elements on the canvas
-function setup() {
-    createCanvas(400, 400);
-    background("black");
 
-    // <<< TODO: Add your code here
-}
+
+            createCanvas(400, 400);
+            background("black");
+
+            fill("yellow")
+            textSize(70)
+            text(randomOperand1, width / 4, 100)
+            text(randomOperand2, width / 2, 100)
+
+
+
+        }
 
 // mouseClicked function checks if the user clicked on the correct answer
 function mouseClicked() {
- let randomOperator: any // <<< TODO: Add your code here
+    // <<< TODO: Add your code here
 }
 
 // mouseMoved function displays mouse coordinates as feedback
