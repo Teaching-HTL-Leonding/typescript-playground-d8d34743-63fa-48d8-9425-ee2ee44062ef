d89d4fb01b6cdac201265597b0101bab2d105fe7
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 10:49:38 +0100

    Saved in TypeScript Playground at 2024-12-04T09:49:38.526Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -1,1 +1,8 @@
-OHHHHHHHHHHHHHHHHHH
+const TOP_MARGIN = 80;
+const SPHERE_DIAMETER = 30;
+const INITIAL_TRIANGLE_BASE_WIDTH = 100;
+const SIZE_INCREASE_BY_LAYER = 0.5;
+
+
+  // <<< Draw the tree
+}
