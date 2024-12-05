c291c1bf8071fa519f4293c04faf27102ecc6154
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 10:30:15 +0100

    Saved in TypeScript Playground at 2024-12-05T09:30:15.031Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -1,22 +1,39 @@
+grid = 10;
+
 function setup() {
     createCanvas(601, 301);
-    background("black");
+        background("black");
 
-    fill("aqua");
-    noStroke();
+            fill("aqua");
+                noStroke();
 
-    textSize(50);
-    textAlign(CENTER, CENTER);
-    text("Move the mouse to start", 0, 0, width, height);
-}
+                    textSize(50);
+                        textAlign(CENTER, CENTER);
+                            text("Move the mouse to start", 0, 0, width, height);
+                            }
 
-function mouseMoved() {
-    resetMatrix();
-    background("black");
-    strokeWeight(2);
 
-    noFill();
-    stroke("aqua");
 
-    // <<< Add your code here
+
+                            function mouseMoved() {
+                                resetMatrix();
+                                    background("black");
+                                        strokeWeight(2);
+                                            noFill();
+                                                stroke("aqua");
+                                                    let u = 10;
+                                                        // <<< Add your code here
+                                                            for (let y = 0; y < height; y += u) {
+
+                                                                    for (let x = 0; x < width; x += u) {
+                                                                                push();
+                                                                                            translate(x, y);
+                                                                                                        line(0, 0, u, u / 2);
+                                                                                                                    line(u, u / 2, 0, u);
+                                                                                                                                pop()
+                                                                                                                                        }
+
+                                                                                                                                            }
+                                                                                                                                            }
+                                                                                                                                            
 }
