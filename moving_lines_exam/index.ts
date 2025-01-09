ebe6ccda2f55cc09134d62de7167487d6fbf3845
Rainer Stropek <rainer@timecockpit.com>
2025-01-09 08:35:47 +0100

    Saved in TypeScript Playground at 2025-01-09T07:35:46.982Z

--- moving_lines_exam/index.ts
+++ moving_lines_exam/index.ts
@@ -6,35 +6,35 @@
 // "max hue" is the maximum hue (HSB color space), can be between 0 and 360
 const CONFIGURATION = "5;0-360";
 
-let lineStartX: number []=[] // Start coordinates of line
-let lineStartY: number []=[]
-let lineEndX: number []=[]     // End coordinates of line
-let lineEndY: number []=[]
-let lineColor: number []=[]
+let lineStartX: number[]=[]; // Start coordinates of line
+let lineStartY: number[]=[];
+let lineEndX: number[]=[];  // End coordinates of line
+let lineEndY: number[]=[];
+let lineColor: number[]=[];
 
-let lineStartDx: number = 0;    // Movement of start point per frame in X and Y direction
-let lineStartDy: number = 0;
-let lineEndDx: number = 0;      // Movement of end point per frame in X and Y direction
-let lineEndDy: number = 0;
+let lineStartDx: number[]=[];  // Movement of start point per frame in X and Y direction
+let lineStartDy: number[]=[];
+let lineEndDx: number[]=[];    // Movement of end point per frame in X and Y direction
+let lineEndDy: number[]=[];
 
 let minColor = 0;               // Lower bound of random hue value
 let maxColor = 360;             // Upper bound of random hue value
 
-function setup() {
+function setup() 
     createCanvas(500, 500);
     colorMode(HSB);
-
+for(let i = 0; i<10;1++){
     // Set random start and end position
-    lineStartX = random(50, 450);
-    lineStartY = random(50, 450);
-    lineEndX = random(50, 450);
-    lineEndY = random(50, 450);
+    lineStartx.push = random(50, 450);
+    lineStarty.push = random(50, 450);
+    lineEndX.push = random(50, 450);
+    lineEndy.push = random(50, 450);
 
     // Set random movement
-    lineStartDx = random(0, 5);
-    lineStartDy = random(0, 5);
-    lineEndDx = random(0, 5);
-    lineEndDy = random(0, 5);
+    lineStartDx.push = random(0, 5);
+    lineStartDy.push = random(0, 5);
+    lineEndDx.push = random(0, 5);
+    lineEndDy.push = random(0, 5);
 
     // Set random color
     lineColor = random(minColor, maxColor); 
