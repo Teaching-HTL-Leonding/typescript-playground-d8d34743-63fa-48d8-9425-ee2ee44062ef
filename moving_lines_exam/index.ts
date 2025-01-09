c15c5e5efbd92cefef5d5f49b1b5da65b398119a
Rainer Stropek <rainer@timecockpit.com>
2025-01-09 09:23:25 +0100

    Saved in TypeScript Playground at 2025-01-09T08:23:24.603Z

--- moving_lines_exam/index.ts
+++ moving_lines_exam/index.ts
@@ -24,52 +24,54 @@ function setup() {
 
     createCanvas(500, 500);
     colorMode(HSB);
-    for (let i = 0; i < 10; i++) 
+    for (let i = 0; i < 10; i++) {
         // Set random start and end position
         lineStartX.push(random(50, 450));
         lineStartY.push(random(50, 450));
-        lineEndX.push (random(50, 450));
-        lineEndY.push (random(50, 450));
+        lineEndX.push(random(50, 450));
+        lineEndY.push(random(50, 450));
 
         // Set random movement
         lineStartDx.push(random(50, 450));
         lineStartDy.push(random(50, 450));
-        lineEndDx.push (random(50, 450));
-        lineEndDy.push (random(50, 450));
+        lineEndDx.push(random(50, 450));
+        lineEndDy.push(random(50, 450));
 
         // Set random color
         lineColor.push(random(minColor, maxColor));
-    }
 
-    function draw() {
-        background("black");
-for (let i = 0; i < 10; i++)
+    }
+}
+
+function draw() {
+    background("black");
+    for (let i = 0; i < 10; i++)
         push();
-        // Draw current line
-        stroke(lineColor[i], 100, 100);
-        strokeWeight(2);
-        line(lineStartX[i], lineStartY[i], lineEndX[i], lineEndY[i]);
 
-        // Move start and end point
-        lineStartX += lineStartDx;
-        lineStartY += lineStartDy;
-        lineEndX += lineEndDx;
-        lineEndY += lineEndDy;
+    stroke(lineColor[i], 100, 100);
+    strokeWeight(2);
+    line(lineStartX[i], lineStartY[i], lineEndX[i], lineEndY[i]);
 
-        // Reverse direction when edge has been reached
-        if (lineStartX < 0 || lineStartX > width) {
-            lineStartDx = -lineStartDx;
-        }
-        if (lineStartY < 0 || lineStartY > height) {
-            lineStartDy = -lineStartDy;
-        }
+    // Move start and end point
+    lineStartX += lineStartDx;
+    lineStartY += lineStartDy;
+    lineEndX += lineEndDx;
+    lineEndY += lineEndDy;
 
-        if (lineEndX < 0 || lineEndX > width) {
-            lineEndDx = -lineEndDx;
-        }
-        if (lineEndY < 0 || lineEndY > height) {
-            lineEndDy = -lineEndDy;
-        }
-
-        pop();
+    // Reverse direction when edge has been reached
+    if (lineStartX[i] < 0 || lineStartX[i] > width) {
+        lineStartDx[i] = -lineStartDx;
     }
+    if (lineStartY[i] < 0 || lineStartY[i] > height) {
+        lineStartDy[i] = -lineStartDy;
+    }
+
+    if (lineEndX[i] < 0 || lineEndX[i] > width) {
+        lineEndDx[i] = -lineEndDx;
+    }
+    if (lineEndY[i] < 0 || lineEndY[i] > height) {
+        lineEndDy[i] = -lineEndDy[i];
+    }
+
+    pop();
+}
