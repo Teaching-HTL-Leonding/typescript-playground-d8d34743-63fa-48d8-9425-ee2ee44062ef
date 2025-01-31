6ebe4f0a4c2eabd5e654b32c70706bbae30858dc
Rainer Stropek <rainer@timecockpit.com>
2025-01-31 12:39:25 +0100

    Saved in TypeScript Playground at 2025-01-31T11:39:25.219Z

---
+++ empty_playground/index.ts
@@ -0,0 +1,14 @@
+const divRes - div(10, 2);
+console.info(divRes);
+
+// Wenn y = 0 -> 0 zurückgeben
+function div(x: number, y: number): number {
+    if (y === 0) {
+
+    }
+
+ const result = x / y;
+ return result;
+  }
+
+  function print(message: string)
